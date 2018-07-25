a4b342356783047a7408c59a15a3f0c9e8e47e8b
yakenohara <38835246+yakenohara@users.noreply.github.com>
2018-07-25 20:51:28 +0900

    no message

---
+++ 05_NowPlaying.js
@@ -0,0 +1,46 @@
+//ActiveXObject生成
+	var axobj = new ActiveXObject("Scripting.FileSystemObject"); //FileSystem
+	var wshobj = new ActiveXObject("WScript.Shell");//WScript
+
+//iTunesObject生成
+	var itobj = WScript.CreateObject("iTunes.Application");
+	var track = itobj.CurrentTrack;    
+	var info;
+
+//ファイル・フォルダ
+	var mydocu = wshobj.SpecialFolders("MyDocuments");//マイドキュメント場所
+	var nowpfol = "NowPlaying";//専用フォルダ名
+	var nowpfil = "NowPlaying.txt";//専用ファイル名
+	
+//フォルダ存在確認
+	if(!(axobj.FolderExists(mydocu + "\\" + nowpfol))){
+		axobj.CreateFolder(mydocu + "\\" + nowpfol);//フォルダ作成
+	}
+
+//曲情報収集
+	try{
+		if(track.Kind == 1){ //ローカル再生中
+			info=track.Artist + " - " + track.Name;
+		}else if(track.Kind == 3) {//ストリーム再生中
+			var titles = itobj.currentStreamTitle.split(",");
+			info=titles[0];
+		}else{
+			WScript.Echo("不明な動作中");
+			WScript.Quit();
+		}
+	}catch(e){
+		WScript.Echo("iTunesが動作していません。");
+	}
+	
+//ファイルオープン
+	try{
+		var txfl = axobj.OpenTextFile(mydocu + "\\" + nowpfol + "\\" + nowpfil,8,true);
+	}catch(e){
+		WScript.Echo(nowpfil + "が開けません。");
+	}
+
+//ファイルへ書き込み
+	txfl.Write(info + "\n");
+	
+//ファイルクローズ
+	txfl.Close();
